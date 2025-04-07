ebed6461a207c02071589a684949b05a4b0f6115
kidde <codyjclose95@gmail.com>
2025-04-07 18:59:34 -0500

    anchiornis

--- groupProject/index.aspx
+++ groupProject/index.aspx
@@ -5,4 +5,5 @@
     <asp:Image ID="img_irritator" runat="server" ImageUrl="~/assets/img/Irritator.jpg" Width="50" Height="100%" />
     <asp:Image ID="Image1" runat="server" ImageUrl="~/assets/img/torvosaurus.jpg" Width="50" Height="100%" />
     <asp:Image ID="Image2" runat="server" ImageUrl="~/assets/img/acrotholus.jpg" Width="50" Height="100%" />
+    <asp:Image ID="Image3" runat="server" ImageUrl="~/assets/img/anchiornis.jpg" Width="50" Height="100%" />
 </asp:Content>
--- groupProject/index.aspx.designer.cs
+++ groupProject/index.aspx.designer.cs
@@ -40,5 +40,14 @@ namespace groupProject
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Image Image2;
+
+        /// <summary>
+        /// Image3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image3;
     }
 }
